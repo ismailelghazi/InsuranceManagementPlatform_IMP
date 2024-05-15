ea1573a145d14ae712f9b26289e54dcc3401947a
Hamza Khiar <hamza.khiar.01@gmail.com>
2024-05-16 00:54:46 +0100

    login mzyan

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,10 +1,24 @@
 
-function App() {
-  return (
-    <div>
-      test
-    </div>
-  );
+function Login() {
+    // 
+    return (
+        <div class="w-screen h-screen bg-cyan-100 flex justify-center items-center">
+            <div class="login-container w-1/3 h-1/4 bg-blue-300 flex flex-col items-center py-4">
+                <h1 class="font-bold text-4xl">Login</h1>
+                <form class="flex flex-col w-3/4 gap-4">
+                    <div class="input-group flex flex-col">
+                        <label for="email">Email:</label>
+                        <input type="text" name="email" />
+                    </div>
+                    <div class="input-group flex flex-col">
+                        <label for="password">Password:</label>
+                        <input type="password" name="password"/>
+                    </div>
+                    <button type="submit" class="w-1/2 py-2 rounded bg-blue-950 text-white mx-auto">Submit</button>
+                </form>
+            </div>
+          </div>
+    );
 }
 
-export default App;
+export default Login;
