4249b12c2f68751a79cd721416c24222312fcf27
Hamza Khiar <hamza.khiar.01@gmail.com>
2024-06-03 06:00:11 +0100

    done upload nav

--- front/src/env.js
+++ front/src/env.js
@@ -1,3 +1,3 @@
-export const BACKEND_URL="http://127.0.0.1:8000/";
-export const BACKEND_URL_API="http://127.0.0.1:8000/api";
+export const BACKEND_URL = "http://127.0.0.1:5300/";
+export const BACKEND_URL_API = "http://127.0.0.1:5300/api";
 
