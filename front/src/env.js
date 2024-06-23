d28fd2ad472e204a7b433595acd8624f590134e7
Hamza Khiar <hamza.khiar.01@gmail.com>
2024-06-23 12:24:56 +0100

    merge

--- front/src/env.js
+++ front/src/env.js
@@ -1,7 +1,7 @@
-export const BACKEND_URL=import.meta.env.VITE_BACKEND_URL;
-export const BACKEND_URL_API=import.meta.env.VITE_BACKEND_URL_API;
+//export const BACKEND_URL=import.meta.env.VITE_BACKEND_URL;
+//export const BACKEND_URL_API=import.meta.env.VITE_BACKEND_URL_API;
 
-//export const BACKEND_URL = "http://127.0.0.1:5300/";
-//export const BACKEND_URL_API = "http://127.0.0.1:5300/api";
+export const BACKEND_URL = "http://127.0.0.1:5300/";
+export const BACKEND_URL_API = "http://127.0.0.1:5300/api";
 
 
