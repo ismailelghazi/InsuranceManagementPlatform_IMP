7d63946a068f1991425e33d06cba7bef5afee868
ismailelghazi <ismail47el@gmail.com>
2024-06-25 13:07:27 +0100

    85.5%

--- front/src/Pages/dashboard.jsx
+++ front/src/Pages/dashboard.jsx
@@ -23,18 +23,32 @@ function Dashboard() {
                     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                         {total() && (
                             <>
-                                <div class="bg-gray-400 w-1/2 rounded">
-                                    <span class="text-3xl bold">Total d'Assure</span>
-                                    <p>{total().total_assures}</p>
+                                <div class="bg-white shadow-lg rounded-lg p-6 hover:shadow-xl transition-shadow duration-300">
+                                    <div class="flex items-center mb-4">
+                                        <AiOutlineDollarCircle className="h-10 w-10 text-blue-600" />
+                                        <div class="ml-4">
+                                            <span class="text-lg font-semibold text-gray-600">Total d'Assure</span>
+                                            <p class="text-2xl font-bold text-gray-800">{total().total_assures}</p>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="bg-gray-400 w-1/2 rounded">
-                                    <span class="text-3xl bold">Total de Produit Issuée</span>
-                                    <p>{total().total_products}</p>
+                                <div class="bg-white shadow-lg rounded-lg p-6 hover:shadow-xl transition-shadow duration-300">
+                                    <div class="flex items-center mb-4">
+                                        <AiOutlineFileText className="h-10 w-10 text-green-600" />
+                                        <div class="ml-4">
+                                            <span class="text-lg font-semibold text-gray-600">Total de Produit Issuée</span>
+                                            <p class="text-2xl font-bold text-gray-800">{total().total_products}</p>
+                                        </div>
+                                    </div>
                                 </div>
-
-                                <div class="bg-gray-400 w-1/2 rounded">
-                                    <span class="text-3xl bold">Total Reglements (Credit/Caisse)</span>
-                                    <p>{total().total_montant}</p>
+                                <div class="bg-white shadow-lg rounded-lg p-6 hover:shadow-xl transition-shadow duration-300">
+                                    <div class="flex items-center mb-4">
+                                        <AiOutlineTeam className="h-10 w-10 text-red-600" />
+                                        <div class="ml-4">
+                                            <span class="text-lg font-semibold text-gray-600">Total Reglements (Crédit/Caisse)</span>
+                                            <p class="text-2xl font-bold text-gray-800">{total().total_montant}</p>
+                                        </div>
+                                    </div>
                                 </div>
                             </>
                         )}
@@ -45,33 +59,4 @@ function Dashboard() {
     );
 }
 
-
-// <div class="bg-white shadow-lg rounded-lg p-6 hover:shadow-xl transition-shadow duration-300">
-//                                     <div class="flex items-center mb-4">
-//                                         <AiOutlineDollarCircle className="h-10 w-10 text-blue-600" />
-//                                         <div class="ml-4">
-//                                             <span class="text-lg font-semibold text-gray-600">Total d'Assure</span>
-//                                             <p class="text-2xl font-bold text-gray-800">{total().total_assures}</p>
-//                                         </div>
-//                                     </div>
-//                                 </div>
-//                                 <div class="bg-white shadow-lg rounded-lg p-6 hover:shadow-xl transition-shadow duration-300">
-//                                     <div class="flex items-center mb-4">
-//                                         <AiOutlineFileText className="h-10 w-10 text-green-600" />
-//                                         <div class="ml-4">
-//                                             <span class="text-lg font-semibold text-gray-600">Total de Produit Issuée</span>
-//                                             <p class="text-2xl font-bold text-gray-800">{total().total_products}</p>
-//                                         </div>
-//                                     </div>
-//                                 </div>
-//                                 <div class="bg-white shadow-lg rounded-lg p-6 hover:shadow-xl transition-shadow duration-300">
-//                                     <div class="flex items-center mb-4">
-//                                         <AiOutlineTeam className="h-10 w-10 text-red-600" />
-//                                         <div class="ml-4">
-//                                             <span class="text-lg font-semibold text-gray-600">Total Reglements (Crédit/Caisse)</span>
-//                                             <p class="text-2xl font-bold text-gray-800">{total().total_montant}</p>
-//                                         </div>
-//                                     </div>
-// 
-// 
 export default Dashboard;
