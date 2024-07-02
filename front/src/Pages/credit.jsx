658841a2665cf39658017ab73356dc4c3b1e55b3
Hamza Khiar <hamza.khiar.01@gmail.com>
2024-07-02 16:28:25 +0100

    credit m9ada

--- front/src/Pages/credit.jsx
+++ front/src/Pages/credit.jsx
@@ -7,7 +7,7 @@ import 'jspdf-autotable';
 function EtatCredit() {
     const [credits, setCredits] = createSignal(null);
     const [searchQuery, setSearchQuery] = createSignal('');
-    const [filteredCredits, setFilteredCredits] = createSignal([]);
+    const [filteredCredits, setFilteredCredits] = createSignal({reglements:[]});
     const [filter, setFilter] = createSignal('all');
     const [headersCount, setHeadersCount] = createSignal(0);
     const [currentPage, setCurrentPage] = createSignal(1);
@@ -49,7 +49,7 @@ function EtatCredit() {
     const paginatedCredits = () => {
         const startIndex = (currentPage() - 1) * itemsPerPage;
         const endIndex = startIndex + itemsPerPage;
-        return filteredCredits().slice(startIndex, endIndex);
+        return filteredCredits().reglements.slice(startIndex, endIndex);
     };
 
     const totalPages = () => Math.ceil(filteredCredits().length / itemsPerPage);
@@ -100,74 +100,75 @@ function EtatCredit() {
         }
         if (searchQuery()) {
             filtered = filtered.filter(credit =>
-                credit.nom_assure.toLowerCase().includes(searchQuery().toLowerCase())
-            );
-        }
-        setFilteredCredits(filtered);
-    };
+            credit.nom_assure.toLowerCase().includes(searchQuery().toLowerCase())
+        );
+    }
+    setFilteredCredits(filtered);
+};
 
-    return (
-        <div class="flex w-screen h-screen bg-gray-100 overflow-hidden">
-            <Navbar />
-            <div class="dashboard-product-container w-full h-full pl-16 py-24">
-                <h1 class="text-3xl md:text-5xl text-blue-900 font-bold mb-8">Reglement Credits</h1>
-                <div class="bg-white shadow-md w-11/12 rounded-lg p-6 mr-12">
-                    <div class="flex flex-col md:flex-row justify-between items-center mb-4">
-                        <h2 class="text-xl md:text-3xl font-semibold text-gray-800 mb-4 md:mb-0">Credits List</h2>
-                        <div class="flex items-center gap-2">
-                            <input
-                                type="text"
-                                placeholder="Search"
-                                class="py-2 px-3 border border-gray-300 rounded-lg mr-4"
-                                value={searchQuery()}
-                                onInput={(e) => setSearchQuery(e.target.value)}
-                                onKeyUp={() => filterCredits(searchQuery(), startDate(), endDate())}
-                            />
-                            <label class="mr-2">Start Date</label>
-                            <input
-                                type="date"
-                                class="py-2 px-3 border border-gray-300 rounded-lg mr-4"
-                                value={startDate()}
-                                onInput={(e) => setStartDate(e.target.value)}
-                                onChange={() => filterCredits(searchQuery(), startDate(), endDate())}
-                            />
-                            <label class="mr-2">End Date</label>
-                            <input
-                                type="date"
-                                class="py-2 px-3 border border-gray-300 rounded-lg mr-4"
-                                value={endDate()}
-                                onInput={(e) => setEndDate(e.target.value)}
-                                onChange={() => filterCredits(searchQuery(), startDate(), endDate())}
-                            />
-                            <select class="py-2 px-3 border border-gray-300 rounded-lg" value={filter()} onChange={(e) => filterDetails(e.target.value)}>
-                                <option value="all">All</option>
-                                <option value="solder">Solder</option>
-                                <option value="encour">Encour</option>
-                            </select>
-                            <button
-                                onClick={generatePDF}
-                                class="py-2 px-4 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
-                            >
-                                Print
-                            </button>
-                        </div>
+return (
+    <div class="flex w-screen h-screen bg-gray-100 overflow-hidden">
+        <Navbar />
+        <div class="dashboard-product-container w-full h-full pl-16 py-24">
+            <h1 class="text-3xl md:text-5xl text-blue-900 font-bold mb-8">Reglement Credits</h1>
+            <div class="bg-white shadow-md w-11/12 rounded-lg p-6 mr-12">
+                <div class="flex flex-col md:flex-row justify-between items-center mb-4">
+                    <h2 class="text-xl md:text-3xl font-semibold text-gray-800 mb-4 md:mb-0">Credits List</h2>
+                    <div class="flex items-center gap-2">
+                        <input
+                            type="text"
+                            placeholder="Search"
+                            class="py-2 px-3 border border-gray-300 rounded-lg mr-4"
+                            value={searchQuery()}
+                            onInput={(e) => setSearchQuery(e.target.value)}
+                            onKeyUp={() => filterCredits(searchQuery(), startDate(), endDate())}
+                        />
+                        <label class="mr-2">Start Date</label>
+                        <input
+                            type="date"
+                            class="py-2 px-3 border border-gray-300 rounded-lg mr-4"
+                            value={startDate()}
+                            onInput={(e) => setStartDate(e.target.value)}
+                            onChange={() => filterCredits(searchQuery(), startDate(), endDate())}
+                        />
+                        <label class="mr-2">End Date</label>
+                        <input
+                            type="date"
+                            class="py-2 px-3 border border-gray-300 rounded-lg mr-4"
+                            value={endDate()}
+                            onInput={(e) => setEndDate(e.target.value)}
+                            onChange={() => filterCredits(searchQuery(), startDate(), endDate())}
+                        />
+                        <select class="py-2 px-3 border border-gray-300 rounded-lg" value={filter()} onChange={(e) => filterDetails(e.target.value)}>
+                            <option value="all">All</option>
+                            <option value="solder">Solder</option>
+                            <option value="encour">Encour</option>
+                        </select>
+                        <button
+                            onClick={generatePDF}
+                            class="py-2 px-4 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
+                        >
+                            Print
+                        </button>
                     </div>
-                    <div class="table-content-product min-w-full">
-                        <div class="table-head grid bg-gray-200 text-gray-700 font-semibold py-2 px-4 rounded-t-lg hidden md:grid"
-                             style="grid-template-columns: repeat(6, 1fr);">
-                            <span class="col-span-1">Date de Reglement</span>
-                            <span class="col-span-1">Police</span>
-                            <span class="col-span-1">Nom Assure</span>
-                            <span class="col-span-1">Montant Reglement</span>
-                            <span class="col-span-1">Type Reglement</span>
-                            <span class="col-span-1">Etat</span>
-                        </div>
-                        <div class="table-body overflow-y-scroll max-h-[600px] styled-scrollbar">
-                            <For each={paginatedCredits()}>
+                </div>
+                <div class="table-content-product min-w-full">
+                    <div class="table-head grid bg-gray-200 text-gray-700 font-semibold py-2 px-4 rounded-t-lg hidden md:grid"
+                         style="grid-template-columns: repeat(7, 1fr);">
+                        <span class="col-span-1">Date d'Emission </span>
+                        <span class="col-span-1">Police</span>
+                        <span class="col-span-1">Nom Assure</span>
+                        <span class="col-span-1">Montant Reglement</span>
+                        <span class="col-span-1">Type Prime</span>
+                        <span class="col-span-1">Etat Credit</span>
+                        <span class="col-span-1">Reste</span>
+                    </div>
+                    <div class="table-body overflow-y-scroll max-h-[600px] styled-scrollbar">
+                        <For each={paginatedCredits()}>
                                 {(item) => (
-                                    <div class="grid py-2 px-4 border-b border-gray-200 gap-y-8 grid-cols-1 md:grid-cols-6">
-                                        <div class="md:hidden font-semibold">Date de Reglement</div>
-                                        <div class="col-span-1 truncate">{item.date_de_reglement}</div>
+                                    <div class="grid py-2 px-4 border-b border-gray-200 gap-y-8 grid-cols-1 md:grid-cols-7">
+                                        <div class="md:hidden font-semibold">Date d'Emission </div>
+                                        <div class="col-span-1 truncate">{item.date_emission}</div>
 
                                         <div class="md:hidden font-semibold">Police</div>
                                         <div class="col-span-1 truncate">{item.police}</div>
@@ -178,10 +179,11 @@ function EtatCredit() {
                                         <div class="md:hidden font-semibold">Montant Reglement</div>
                                         <div class="col-span-1 truncate">{item.montant_reglement}</div>
 
-                                        <div class="md:hidden font-semibold">Type Reglement</div>
-                                        <div class="col-span-1 truncate">{item.type_reglement}</div>
+                                        <div class="md:hidden font-semibold">Total Prime</div>
+                                        <div class="col-span-1 truncate">{item.total_prime_totale}</div>
 
-                                        <div class="col-span-1 truncate">{item.etat}</div>
+                                        <div class="col-span-1 truncate">{item.etat_credit}</div>
+                                        <div class="col-span-1 truncate">{item.reste}</div>
                                     </div>
                                 )}
                             </For>
